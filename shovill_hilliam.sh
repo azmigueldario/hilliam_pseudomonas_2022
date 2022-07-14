af3ec174912e7f47c27bf77bd89645f45b771d34
mdprieto <azmigueldario@gmail.com>
2022-07-14 11:44:27 -0700

    "finalized shovill script for all 190 samples"

--- shovill_hilliam.sh
+++ shovill_hilliam.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 #SBATCH --account=def-whsiao-ab
-#SBATCH --mem-per-cpu=8G # 8 GB of memory per cpu core
-#SBATCH --time=02:00:00
-#SBATCH --ntasks=4 # run four tasks in parallel
-#SBATCH --cpus-per-task=4 # use 4 CPU cores per task
-#SBATCH --job-name="shovill assembly hilliam data"
+#SBATCH --mem-per-cpu=14G #  GB of memory per cpu core
+#SBATCH --time=22:45:00
+#SBATCH --ntasks=1 # tasks in parallel
+#SBATCH --cpus-per-task=20 # use 16 CPU cores per task
+#SBATCH --job-name="shovill_assembly_hilliam"
 #SBATCH --chdir=/scratch/mdprieto/
+#SBATCH --output=slurm_shovill.out
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 ################################## preparation #########################################
 
 # load singularity to execute shovill
-module purge 
+module purge
 module load singularity/3.8
 
 # mount my filesystem inside container
 # ---------- localscratch is defined to use compute node temp folder
-BIND_MOUNT="-B /home -B /project -B /scratch -B /localscratch:/temp"
+BIND_MOUNT="-B /home -B /project -B /scratch -B /localscratch -B /localscratch:/temp"
 
 # create variables and output dir
 mkdir -p /scratch/mdprieto/results_hilliam/shovill
@@ -25,12 +26,16 @@ INPUT_DIR="/project/6056895/mdprieto/hilliam_pseudomonas/bronchiectasis_reads"
 
 ################################## shovill #########################################
 
-for file1 in $(ls $INPUT_DIR/*R1*fastq.gz | head -n 19)
+for file1 in $(ls $INPUT_DIR/*R1*fastq.gz)
+
 do
     file2=${file1/R1/R2}
     trimmed=${file1/R1/R0}
+    out_dir_sample=$(echo $file1 | grep -o '[0-9]*-C[0-9]*') 
     singularity exec $BIND_MOUNT shovill.sif shovill --R1 $file1 --R2 $file2 \
-    --outdir $OUTPUT_DIR \
+    --outdir $OUTPUT_DIR/$out_dir_sample \
     --opts "-s $trimmed" \
-    --ram 16 
+    --force \
+    --cpus 20 \
+    --ram 270
 done
