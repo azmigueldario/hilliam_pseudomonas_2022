a1c384efa3c7da13f20141c9c636526413e5835f
mdprieto <azmigueldario@gmail.com>
2022-07-25 19:28:57 -0700

    "added files with PAG and updated scripts
    - Working version of assembly with shovill script
    - Added scripts to prepare databases for BLAST and run blast against
      homemade databases
    - Added results of BLAST with only one hit per database per query
    - Reference text files with pathogen associated genes (PAG) from
      pseudomonas isolates
    - Added fasta file with sequences of Pseudomonas PAG"

---
+++ scripts/blast_hilliam.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --account=def-whsiao-ab
+#SBATCH --mem-per-cpu=4G #  GB of memory per cpu core
+#SBATCH --time=00:15:00
+#SBATCH --ntasks=1 # tasks in parallel
+#SBATCH --cpus-per-task=4 # CPU cores per task
+#SBATCH --job-name="blast_pags_hilliam"
+#SBATCH --chdir=/scratch/mdprieto/
+#SBATCH --output=blast_pags_hilliam.out
+
+###########################################################################
+
+# load blast+ module
+module purge
+module load StdEnv/2020  gcc/9.3.0 blast+/2.12.0
+	
+tblastn -query /home/mdprieto/git/hilliam_pseudomonas_2022/pags_fasta.fa \
+	-db /scratch/mdprieto/results_hilliam/blastdb/hilliam_assemblies \
+	-show_gis \
+	-outfmt 6 \
+	-out /scratch/mdprieto/hilliam_blast_full.txt \
+	-evalue 1e-50 \
+	-num_threads 4 \
+	-max_hsps 1
--- scripts/shovill_hilliam.sh
+++ scripts/shovill_hilliam.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #SBATCH --account=def-whsiao-ab
-#SBATCH --mem-per-cpu=14G #  GB of memory per cpu core
-#SBATCH --time=22:45:00
+#SBATCH --mem-per-cpu=12G #  GB of memory per cpu core
+#SBATCH --time=02:00:00
 #SBATCH --ntasks=1 # tasks in parallel
-#SBATCH --cpus-per-task=20 # use 16 CPU cores per task
+#SBATCH --cpus-per-task=16 # CPU cores per task
 #SBATCH --job-name="shovill_assembly_hilliam"
 #SBATCH --chdir=/scratch/mdprieto/
-#SBATCH --output=slurm_shovill.out
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+#SBATCH --output=slurm_shovill_16x12.out
+#SBATCH --mail-user=mprietog@sfu.ca
+#SBATCH --mail-type=END
 
 ################################## preparation #########################################
 
@@ -29,13 +30,20 @@ INPUT_DIR="/project/6056895/mdprieto/hilliam_pseudomonas/bronchiectasis_reads"
 for file1 in $(ls $INPUT_DIR/*R1*fastq.gz)
 
 do
+    # create environment variables for R2 and R0 files and establish a name for the output directory
     file2=${file1/R1/R2}
     trimmed=${file1/R1/R0}
-    out_dir_sample=$(echo $file1 | grep -o '[0-9]*-C[0-9]*') 
+    out_dir_sample=$(echo $file1 | grep -oE '[0-9]{1,3}-[ABC][0-9]*')
+    
+    # ------ Execute shovill inside singularity container
+    # --opts = options to pass into spades assembler
+    # --ram = total ram in all CPUs
+    
     singularity exec $BIND_MOUNT shovill.sif shovill --R1 $file1 --R2 $file2 \
     --outdir $OUTPUT_DIR/$out_dir_sample \
     --opts "-s $trimmed" \
-    --force \
-    --cpus 20 \
-    --ram 270
+    --cpus $SLURM_CPUS_PER_TASK \
+    --ram 140 \
+    --tmpdir /scratch/mdprieto/tmp
+    echo "Finished assembly of sample"
 done
